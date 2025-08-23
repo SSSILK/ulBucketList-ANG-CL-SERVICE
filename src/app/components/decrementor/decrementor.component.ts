ed569a671cc66f75261c51c89f1845eea2f6e989
Daijohn Bryant <ohnntsendto@gmail.com>
2025-08-23 21:33:50 +0000

    Lab --Completed

--- src/app/components/decrementor/decrementor.component.ts
+++ src/app/components/decrementor/decrementor.component.ts
@@ -13,7 +13,7 @@ export class DecrementorComponent implements OnInit {
   /**
    * TODO: the CountService should be injected into this component as is done in the incrementor component.
    */
-  constructor() { }
+  constructor(private countservice: CountService) { }
 
   ngOnInit(): void {
 
@@ -25,6 +25,7 @@ export class DecrementorComponent implements OnInit {
    * just as has been done in the incrementor component.
    */
   emitDecrementEvent() {
+    this.countservice.decrementNum();
     this.decrementEvent.emit();
   }
 }
